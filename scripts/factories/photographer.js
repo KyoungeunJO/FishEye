ece6cc4fcf2cfa65381610d6480ee1c8e8134dae
Kyoungeun JO <hke3259@naver.com>
2022-05-18 21:01:13 +0200

    Feature: Fetch json data & display photographers on index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,20 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const p = document.createElement( 'p' );
+        p.setAttribute("class", "location");
+        p.textContent = `${city}, ${country}`;
+        const p2 = document.createElement( 'p' );
+        p2.setAttribute("class", "tagline");
+        p2.textContent = tagline;
+        const p3 = document.createElement( 'p' );
+        p3.setAttribute("class", "price");
+        p3.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(p);
+        article.appendChild(p2);
+        article.appendChild(p3);
         return (article);
     }
     return { name, picture, getUserCardDOM }
