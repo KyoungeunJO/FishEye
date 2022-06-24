685962b6c0ab1f6e37e810284e0c4bd567c3f8a4
Kyoungeun JO <hke3259@naver.com>
2022-06-24 18:55:04 +0200

    Feature: check inputs to send contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -16,3 +16,24 @@ function closeModal() {
     {once: true}
     )
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let fields = document.querySelectorAll('input')
+    console.log(fields)
+    let valid = true
+    let response = 'Form sent \n'
+    fields.forEach(i => {
+        valid &= i.checkValidity()
+    })
+    if (valid) {
+        fields.forEach(i => {
+            response += `${i.name}: ${i.value} \n`
+        })
+        form.reset()
+        console.log(response)
+        closeModal()
+    }
+})
